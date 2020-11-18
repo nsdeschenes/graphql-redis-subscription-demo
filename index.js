879c599948e21f21a27b0d87dbd78bbf48bab0d8
nsdeschenes <nsdeschenes@outlook.com>
2020-11-18 09:31:07 -0400

    some progress

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const http = require('http')
 const redis = require('redis')
 const { promisify } = require('util')
 const { RedisPubSub } = require('graphql-redis-subscriptions')
-const PORT = 4000
-
-const { Server } = require('./src/server')
-
-const { SubscriptionServer } = require('subscriptions-transport-ws')
 const { GraphQLSchema, execute, subscribe } = require('graphql')
+const { createServer } = require('graphql-ws')
+
 const { query } = require('./src/query')
 const { mutation } = require('./src/mutation')
 const { subscription } = require('./src/subscription')
+
+const express = require('express')
+
+const { graphqlHTTP } = require('express-graphql')
 
 const schema = new GraphQLSchema({
   query: query,
@@ -32,20 +34,45 @@ const schema = new GraphQLSchema({
   const getAsync = promisify(client.get).bind(client)
   const setAsync = promisify(client.set).bind(client)
 
-  Server({ pubsub, redis: { redis, client, getAsync, setAsync } }).listen(
-    PORT,
-    () => {
-      createServer(
-        {
-          schema,
-          execute,
-          subscribe,
-        },
-        {
-          server: this,
-          path: '/graphql',
-        },
-      )
-    },
+  const app = express()
+
+  app.get('/alive', (_req, res) => {
+    res.json({ ok: 'yes' })
+  })
+  app.get('/ready', (_req, res) => {
+    res.json({ ok: 'yes' })
+  })
+
+  app.use(
+    '/graphql',
+    graphqlHTTP(async (request, response, graphqlParams) => ({
+      graphiql: false,
+      schema,
+      context: {
+        request,
+        response,
+        graphqlParams,
+        pubsub,
+        redis: { redis, client, getAsync, setAsync },
+      },
+    })),
   )
+
+  const server = http.createServer(app)
+
+  server.listen(3000, () => {
+    createServer(
+      {
+        schema,
+        execute,
+        subscribe,
+      },
+      {
+        server,
+        path: '/graphql',
+      },
+    )
+  })
+
+  console.info('Listening on http://localhost:3000/graphql')
 })()
